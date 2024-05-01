d9a7d9cfb0fdc7c2617e87b3ed323baf518a838f
Pablo V. from Nitro-5 <vascor.pablo@gmail.com>
2024-05-01 18:37:41 -0400

    Clase#4 Animaciones

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,6 +20,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -29,17 +35,17 @@ import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 
 private  val messages: List<MyMessage> = listOf(
-    MyMessage(title = "Hola Jetpack Compose", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 2", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 3", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 4", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 5", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 6", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 7", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 8", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 9", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 10", body = "¿Preparado?"),
-    MyMessage(title = "Hola Jetpack Compose 11", body = "¿Preparado?"),
+    MyMessage(title = "Hola Jetpack Compose", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae urna ullamcorper, sollicitudin ante nec, fermentum nisl. Maecenas non metus vel felis blandit bibendum."),
+    MyMessage(title = "Hola Jetpack Compose 2", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    MyMessage(title = "Hola Jetpack Compose 3", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    MyMessage(title = "Hola Jetpack Compose 4", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    MyMessage(title = "Hola Jetpack Compose 5", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    MyMessage(title = "Hola Jetpack Compose 6", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae urna ullamcorper, sollicitudin ante nec, fermentum nisl. Maecenas non metus vel felis blandit bibendum. Nullam risus justo, pulvinar non enim sed, fermentum sodales sapien."),
+    MyMessage(title = "Hola Jetpack Compose 7", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet."),
+    MyMessage(title = "Hola Jetpack Compose 8", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum."),
+    MyMessage(title = "Hola Jetpack Compose 9", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem."),
+    MyMessage(title = "Hola Jetpack Compose 10", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras justo odio, dapibus ac facilisis in, egestas eget quam."),
+    MyMessage(title = "Hola Jetpack Compose 11", body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.")
 
 
 
@@ -97,28 +103,41 @@ fun MyImage() {
 
 @Composable
 fun MyTexts(message: MyMessage) {
-    Column(modifier = Modifier.padding(start = 8.dp)) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(start = 8.dp)
+                           .clickable {
+                               expanded = !expanded
+                           }
+    ) {
         MyText(
             text = message.title,
-            color = MaterialTheme.colorScheme.primary)
+            color = MaterialTheme.colorScheme.primary,
+            lines = 1
+        )
         Spacer(modifier = Modifier.height(16.dp))
         MyText(
             text = message.body,
-            color = MaterialTheme.colorScheme.background )
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            if (expanded) Int.MAX_VALUE else 1
+
+        )
     }
 }
 
 @Composable
-fun MyText(text: String, color: Color) {
-    Text(text, color = color  )
+fun MyText(text: String, color: Color, lines: Int) {
+    Text(text, color = color, maxLines =  lines)
 }
 
 
 @Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
 @Composable
 fun PreviewComponent() {
-    JetpackComposeTutorialTheme {
+
         MyMessages(messages)
-    }
+
 
 }
